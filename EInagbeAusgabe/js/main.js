34acb7370d8e1a825a7bdfa2706663f605ad47c5
ChristophLichtblau <christophlichtblau@web.de>
2024-11-05 09:39:05 +0100

    Złoty in Euro Funktion

--- EInagbeAusgabe/js/main.js
+++ EInagbeAusgabe/js/main.js
@@ -1,8 +1,15 @@
 $(document).ready(function() {
 	// when document loaded:
+	
+
+	$("#titel").html("Złoty in Euro");
 
 	$("#button").on("mouseup",function() {
-
+		$("#ausgabe").html(verdoppeln);
 	});
 
+
+	function verdoppeln(){
+		return $("#eingabe").val() / 4.36 ;
+	}
 });
