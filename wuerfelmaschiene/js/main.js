715297f4a7a0efc283a785c0602a058765dccb77
ChristophLichtblau <christophlichtblau@web.de>
2024-11-06 11:34:06 +0100

    no message

--- wuerfelmaschiene/js/main.js
+++ wuerfelmaschiene/js/main.js
@@ -1,19 +1,49 @@
 $(document).ready(function() {
-	// when document loaded:
-
-	$("#button").on("mouseup",function() {
-
-		$("#wuerfelzahl").html("");
-		$("#wuerfelbild").attr("src","img/wuerfel0.png");
-		 let sound = new Audio("sound/wuerfeln.mp3");
-		 sound.play();
-		setTimeout(wuerfelanzeigen,1000)
+	// Punkte am Anfang auf 0 setzen
+	let punkte = 0;
+  
+	// Event-Handler für den Button
+	$("#button").on("mouseup", function() {
+	  // Anzeige zurücksetzen
+	  $("#wuerfelzahl").html("");
+	  $("#wuerfelbild1").attr("src", "img/wuerfel0.png");
+	  $("#wuerfelbild2").attr("src", "img/wuerfel0.png");
+  
+	  // Sound abspielen
+	  let sound = new Audio("sound/wuerfeln.mp3");
+	  sound.play();
+  
+	  // Funktion `wuerfelanzeigen` nach einer Sekunde aufrufen
+	  setTimeout(wuerfelanzeigen, 1000);
 	});
-
-});
-function wuerfelanzeigen(){
-	let zahl = Math.floor(Math.random()*6)+1;
-		$("#wuerfelzahl").html(zahl);
-		
-			$("#wuerfelbild").attr("src","img/wuerfel"+ zahl + ".png");
-}
+	$("#wuerfelzahl").mouseup(function(){
+		$("#wuerfelzahl").html(0);
+		$("#wuerfelbild1").attr("src", "img/wuerfel0.png");
+	  $("#wuerfelbild2").attr("src", "img/wuerfel0.png");
+	  punkte=0;
+	});
+  
+	// Funktion zum Anzeigen der Würfelzahlen und zum Berechnen der Punkte
+	function wuerfelanzeigen() {
+	  let zahl1 = Math.floor(Math.random() * 6) + 1;
+	  let zahl2 = Math.floor(Math.random() * 6) + 1;
+  
+	  // Bilder für die gewürfelten Zahlen anzeigen
+	  $("#wuerfelbild1").attr("src", "img/wuerfel" + zahl1 + ".png");
+	  $("#wuerfelbild2").attr("src", "img/wuerfel" + zahl2 + ".png");
+  
+	  // Punkte aktualisieren
+	  punkte += zahl1 + zahl2;
+	  
+  
+	   Optional: 
+	  if (zahl1 === zahl2) {
+		punkte = 0;
+		$("#wuerfelzahl").html(punkte);
+	  } else {
+		$("#wuerfelzahl").html(punkte);
+	  }
+	  
+	}
+  });
+  
