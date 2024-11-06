a7d90ed3fa2a00b99004ed264658e939ad96cccd
ChristophLichtblau <christophlichtblau@web.de>
2024-11-06 07:53:00 +0100

    setzten werden beim entern gehighlightet; Feld für das Anzeigen der Aufgabe nach klicken auf = eingefügt

--- Taschenrechner/js/main.js
+++ Taschenrechner/js/main.js
@@ -1,8 +1,10 @@
 $(document).ready(function() {
 	// when document loaded:
 
+	
 	$("#display").on("mousedown",function() {
 		$(this).html("");
+		$("#aufgabe").html("");
 	});
 
 	$(".taste").mouseup(function() {
@@ -14,7 +16,32 @@ $(document).ready(function() {
 	$("#gleich").on("mousedown",function() {
 		
 		let ergebnis = $("#display").html();
+		$("#aufgabe").html($("#display").html());
 		$("#display").html(eval(ergebnis));
+	});
+	
+	$(".taste").mouseenter(function () {
+		$(this).css("background-color","darkgrey");
+
+	});
+	$(".taste").mouseleave(function () {
+		$(this).css("background-color","rgb(72, 72, 72)");
+
+	});
+	$("#gleich").mouseenter(function () {
+		$(this).css("background-color","darkgrey");
+
+	});
+	$("#gleich").mouseleave(function () {
+		$(this).css("background-color","rgb(72, 72, 72)");
+
+	});
+	$("#display").mouseenter(function () {
+		$(this).css("background-color","rgb(200, 228, 170)");
+
+	});
+	$("#display").mouseleave(function () {
+		$(this).css("background-color","rgb(218, 248, 190)");
 
 	});
 
